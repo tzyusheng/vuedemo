dd90b51f8c2a4725662723ad2bbbbac49dfa9865
Your Name <you@example.com>
2023-02-06 17:14:22 +0800

    后端接口

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router/router";
-import svgIcon from "./components/SvgIcon.vue";
-// import App from "./index";
-
-createApp(App).use(router).component("SvgIcon", svgIcon).mount("#app");
